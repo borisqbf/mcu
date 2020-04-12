bca46ddd554e02cd9543a87b52c515663f189e57
borisqbf <boris_qbf@hotmail.com>
2020-04-12 10:56:10 +1000

    increased stability

--- signal_strength8266OLED/signal_strength8266OLED.ino
+++ signal_strength8266OLED/signal_strength8266OLED.ino
@@ -7,7 +7,7 @@
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin) (no difference)
 #define SSD1306_I2C_ADDRESS 0x3C
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
@@ -30,8 +30,8 @@ void setup()
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   display.begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS, false);
  
-  delay(2000);
-  Send1306Command(0xE4);
+//  delay(2000);
+//  Send1306Command(0xE4);
   display.clearDisplay();
   display.display();
 }
