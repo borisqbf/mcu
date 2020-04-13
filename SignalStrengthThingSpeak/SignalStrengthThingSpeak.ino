72571bc0a5a7bf27291c372efac3f5db206c4dfd
borisqbf <boris_qbf@hotmail.com>
2020-04-13 16:06:45 +1000

    replaced serial with RGB LED

--- SignalStrengthThingSpeak/SignalStrengthThingSpeak.ino
+++ SignalStrengthThingSpeak/SignalStrengthThingSpeak.ino
@@ -6,13 +6,28 @@ const char * myWriteAPIKey = "QF3O2AHAVC09JUSZ";
 
 #include <ESP8266WiFi.h>
 
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+
+enum LedColour {
+  RED,
+  GREEN,
+  BLUE
+};
+
 char ssid[] = "QBF";   // your network SSID (name)
 char pass[] = "!QbfReward00";   // your network password
 
 WiFiClient  client;
 
 void setup() {
-  Serial.begin(115200);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+  digitalWrite(D5, HIGH);
+  digitalWrite(D6, HIGH);
+  digitalWrite(D7, HIGH);
   delay(100);
   WiFi.mode(WIFI_STA);
   ThingSpeak.begin(client);
@@ -20,16 +35,15 @@ void setup() {
 
 void reportSignalStrength() {
   // Connect or reconnect to WiFi
+  reportStatus(BLUE, 3);
   if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
     while (WiFi.status() != WL_CONNECTED) {
       WiFi.begin(ssid, pass); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
-      Serial.print(".");
+
       delay(5000);
     }
-    Serial.println("\nConnected.");
   }
+  reportStatus(GREEN, 3);
 
   // Measure Signal Strength (RSSI) of Wi-Fi connection
   long rssi = WiFi.RSSI();
@@ -37,17 +51,22 @@ void reportSignalStrength() {
   // Write value to Field 1 of a ThingSpeak Channel
   int httpCode = 0;
   int retryCount = 0;
+  reportStatus(BLUE, 1);
+
   do {
     httpCode = ThingSpeak.writeField(myChannelNumber, 1, rssi, myWriteAPIKey);
 
     if (httpCode != 200) {
-      Serial.println("Problem writing to channel. HTTP error code " + String(httpCode));
+      reportStatus(RED, 1);
       retryCount++;
       if (retryCount > 10) {
-        Serial.println("Giving up retry atempts");
+        reportStatus(RED, 5);
         break;
       }
+    } else {
+      reportStatus(GREEN, 1);
     }
+
     delay (30 * 1000);
 
   } while (httpCode != 200);
@@ -55,5 +74,22 @@ void reportSignalStrength() {
 void loop() {
   reportSignalStrength();
   // Wait 30 minutes to update the channel again
-  delay(1000 * 60 * 30);
+  delay(1000 *  30);
+}
+
+void reportStatus(LedColour c, int n) {
+  uint8_t pin;
+  if (c == GREEN)
+    pin = D6;
+  else if (c == RED)
+    pin = D5;
+  else
+    pin = D7;
+
+  for (int i = 0; i < n; i++) {
+    digitalWrite(pin, LOW);
+    delay(300);
+    digitalWrite(pin, HIGH);
+    delay(500);
+  }
 }
