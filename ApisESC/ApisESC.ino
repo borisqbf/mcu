eeb1045782d4ea9866974228b518dffcf3f0b81b
borisqbf <boris_qbf@hotmail.com>
2023-01-22 21:28:40 +1100

    control two motors

--- ApisESC/ApisESC.ino
+++ ApisESC/ApisESC.ino
@@ -1,24 +1,37 @@
 
 #include <Servo.h>
 
-byte servoPin = 9; // signal pin for the ESC.
+byte servoPinL = 9; // signal pin for the ESC.
+byte servoPinR = 8; // signal pin for the ESC.
 byte controlPin = 7;
-Servo servo;
+Servo servoL;
+Servo servoR;
 
-void setup() {
-pinMode (7, INPUT);
-servo.attach(servoPin);
-servo.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC.
+void setup()
+{
+  pinMode(7, INPUT);
+  servoL.attach(servoPinL);
+  servoR.attach(servoPinR);
+  servoL.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC.
+  servoR.writeMicroseconds(1500);
+  .
 
-delay(7000); // delay to allow the ESC to recognize the stopped signal.
+      delay(7000); // delay to allow the ESC to recognize the stopped signal.
 }
 
-void loop() {
+void loop()
+{
 
-int controlVal = digitalRead(controlPin); // read input from potentiometer.
+  int controlVal = digitalRead(controlPin); // read input from potentiometer.
 
-if (controlVal == LOW)
-  servo.writeMicroseconds(1800); // Send signal to ESC.
+  if (controlVal == LOW)
+  {
+    servoL.writeMicroseconds(2000); // Send signal to ESC.
+    servoR.writeMicroseconds(2000); 
+  }
   else
-  servo.writeMicroseconds(1500); 
+  {
+    servoL.writeMicroseconds(1500);
+    servoR.writeMicroseconds(1500);
+  }
 }
