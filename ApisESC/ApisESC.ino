f9d249f79af0ba9b58d13e02a55a4cef5b51b65b
borisqbf <boris_qbf@hotmail.com>
2023-02-01 14:12:54 +1100

    Update ApisESC.ino
    added a delay in startup to allow ESC to fully bootup

--- ApisESC/ApisESC.ino
+++ ApisESC/ApisESC.ino
@@ -4,30 +4,31 @@
 byte servoPinL = 9; // signal pin for the ESC.
 byte servoPinR = 8; // signal pin for the ESC.
 byte controlPin = 7;
+unsigned long lastUpdate = 0;
 Servo servoL;
 Servo servoR;
+
+int speed = 1950;
 
 void setup()
 {
   pinMode(7, INPUT);
+  delay(7000);// wait untill ESCs boot up
   servoL.attach(servoPinL);
   servoR.attach(servoPinR);
   servoL.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC.
   servoR.writeMicroseconds(1500);
-  .
-
-      delay(7000); // delay to allow the ESC to recognize the stopped signal.
+  delay(7000); // delay to allow the ESC to recognize the stopped signal.
 }
 
 void loop()
 {
-
   int controlVal = digitalRead(controlPin); // read input from potentiometer.
 
   if (controlVal == LOW)
   {
-    servoL.writeMicroseconds(2000); // Send signal to ESC.
-    servoR.writeMicroseconds(2000); 
+      servoL.writeMicroseconds(speed); // Send signal to ESC.
+      servoR.writeMicroseconds(speed); 
   }
   else
   {
