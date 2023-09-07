14fa9d7b442aa02525fdc35067849e1d922ebc43
borisqbf <boris_qbf@hotmail.com>
2023-09-07 14:20:06 +1000

    test project to call APIs

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Threading;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        using HttpClient client = new();
+
+        while (true)
+        {
+            await ProcessONWebRequestAsync(client);
+            Thread.Sleep(15000);
+            await ProcessOFFWebRequestAsync(client);
+            Thread.Sleep(15000);
+
+
+        }
+        static async Task ProcessONWebRequestAsync(HttpClient client)
+        {
+            var resp = await client.GetStringAsync(
+              "http://192.168.0.54/ON");
+
+            Console.Write(resp);
+
+        }
+
+        static async Task ProcessOFFWebRequestAsync(HttpClient client)
+        {
+            var resp = await client.GetStringAsync(
+              "http://192.168.0.54/OFF");
+
+            Console.Write(resp);
+        }
+    }
+}
